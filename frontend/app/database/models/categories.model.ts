cb434cbc9769bcf18e9a9ed476defb6f108c2b9d
oushima <s1162762@student.windesheim.nl>
2024-04-23 10:40:52 +0200

    feat: category table added.

---
+++ frontend/app/database/models/categories.model.ts
@@ -0,0 +1,14 @@
+import { CategoriesInterface } from '@/app/interfaces/categories.interface';
+import { AuditableInterface } from '@/app/interfaces/index.interface';
+import { Model } from '@nozbe/watermelondb';
+import { date, field } from '@nozbe/watermelondb/decorators';
+
+class CategoriesModel extends Model implements AuditableInterface, CategoriesInterface {
+  static readonly table = 'categories';
+
+  @field('name') name!: string;
+  @date('created_at') updatedAt!: number;
+  @date('updated_at') createdAt!: number;
+}
+
+export default CategoriesModel;
