e0c7c80f16c716bf0b6c3900138b63c69b972202
dennis9284 <oushima@MacBook.local>
2024-05-13 15:47:06 +0200

    feat: repopulate checkboxes

--- frontend/app/hooks/categories-has-ingredients.service.ts
+++ frontend/app/hooks/categories-has-ingredients.service.ts
@@ -1,27 +1,36 @@
-import { CheckboxInterface } from "@interfaces";
-import { CategoriesHasIngredientsModel, IngredientsModel } from "@models";
+import { CategoriesHasIngredientsDTO, CheckboxInterface } from "@interfaces";
+import { CategoriesHasIngredientsModel, CategoriesModel, IngredientsModel } from "@models";
 import { Collection, Q } from "@nozbe/watermelondb";
 import { getAllCategoriesWithIngredients } from "@services";
 import { database } from "app/database/database-setup";
 
 async function allCategoriesWithIngredientsWithCheckboxes() {
     const categoriesWithIngredients = await getAllCategoriesWithIngredients();
+    const categoryHasIngredientsCollection = database.collections.get('categories_has_ingredients') as Collection<CategoriesHasIngredientsModel>;
 
-    return categoriesWithIngredients.map(categoryWithIngredients => {
+    return Promise.all(categoriesWithIngredients.map(async (categoryWithIngredients) => {
+        const ingredients = await Promise.all(categoryWithIngredients.ingredients.map(async (ingredient) => {
+            const checkedRecords = await categoryHasIngredientsCollection.query(
+                Q.where('ingredient_id', ingredient.id)
+            ).fetch();
+
+            const checked = checkedRecords.length > 0 ? checkedRecords[0].checked : false;
+
+            return {
+                id: ingredient.id,
+                label: ingredient.name,
+                value: ingredient as IngredientsModel,
+                checked: checked,
+            };
+        }));
+
         return {
             id: categoryWithIngredients.id,
             label: categoryWithIngredients.name,
-            value: categoryWithIngredients.ingredients.map(ingredient => {
-                return {
-                    id: ingredient.id,
-                    label: ingredient.name,
-                    value: ingredient as IngredientsModel,
-                    checked: false,
-                }
-            }),
+            value: ingredients,
             checked: false,
         };
-    });
+    }));
 }
 
 async function insertOrUpdateCategoryIngredientRelation(newValue: CheckboxInterface): Promise<void> {
