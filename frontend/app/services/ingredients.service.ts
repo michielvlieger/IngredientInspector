ee8c573b242690484966d60c80bd62f3ac75d817
Michiel Vlieger <michiel.vlieger@gmail.com>
2024-05-24 13:08:26 +0200

    returns just one ingredient

--- frontend/app/services/ingredients.service.ts
+++ frontend/app/services/ingredients.service.ts
@@ -3,8 +3,9 @@ import { database } from "../database/database-setup";
 import { IngredientsModel } from "@models";
 import { Q } from "@nozbe/watermelondb";
 
-async function getIngredientByKey(findByKey: string): Promise<IngredientsDTO[]> {
-    return await database.get<IngredientsModel>('ingredients').query(Q.where('key', findByKey)).fetch();
+async function getIngredientByKey(findByKey: string): Promise<IngredientsDTO> {
+    const ingredients = await database.get<IngredientsModel>('ingredients').query(Q.where('key', findByKey)).fetch();
+    return ingredients[0]
 }
 
 export default getIngredientByKey;
